ccdce32fcb08963966522779677d1996cd5c634b
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-07-07 21:51:36 +0300

    Added factorio_concepts module
    All Factorio-related structs are moved here. Also imports were tidied up

---
+++ src/factorio_concepts.rs
@@ -0,0 +1,170 @@
+use std::ffi::OsStr;
+use std::fs::DirEntry;
+use std::cmp::Ordering;
+use lexical_sort::natural_only_alnum_cmp;
+use semver::Version;
+use serde::Deserialize;
+use crate::ModDataErr;
+use crate::dependency::{ModDependencyType, ModDependency};
+
+// enum for states of a mod (enabled or disabled)
+#[derive(Debug)]
+pub enum ModEnabledType {
+    Disabled,
+    Latest,           // Legacy from factorio_mod_manager, probably will be renamed
+    Version(Version), // Legacy from factorio_mod_manager, probably will be removed
+}
+
+// Mod struct, containing mod name, version and enabled info
+#[derive(Debug)]
+pub struct Mod {
+    pub name: String,
+    pub version: Option<ModVersion>,
+    pub enabled: ModEnabledType,
+}
+
+// impls for sorting the mod list for loading order
+impl PartialOrd for Mod {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.name == other.name {
+            return None;
+        }
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Mod {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.has_dependency(&other.name) {
+            return Ordering::Greater;
+        } else {
+            return natural_only_alnum_cmp(&self.name, &other.name);
+        }
+    }
+}
+
+impl PartialEq for Mod {
+    fn eq(&self, other: &Self) -> bool {
+        self.version == other.version && self.name == other.name
+    }
+}
+
+impl Eq for Mod {}
+
+impl Mod {
+    // Check if this mod has other mod as a dependency
+    fn has_dependency(&self, dep_name: &String) -> bool {
+        match &self.version {
+            Some(version) => {
+                for dependency in &version.dependencies {
+                    if &dependency.name == dep_name {
+                        match &dependency.dep_type {
+                            ModDependencyType::Optional | ModDependencyType::Required | ModDependencyType::OptionalHidden => true,
+                            _ => false,
+                        };
+                    }
+                }
+                false
+            },
+            _ => false,
+        }
+    }
+}
+
+// Struct for Mod version (or file, terminology isn't perfect)
+#[derive(Debug)]
+pub struct ModVersion {
+    pub entry: DirEntry,
+    pub dependencies: Vec<ModDependency>,
+    pub structure: ModStructure,
+    pub version: Version,
+}
+
+// impls for comparing mod versions
+impl PartialOrd for ModVersion {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.version.partial_cmp(&other.version) {
+            Some(Ordering::Equal) => {
+                match (&self.structure, &other.structure) {
+                    (ModStructure::Zip, ModStructure::Directory) | (ModStructure::Zip, ModStructure::Symlink) => Some(Ordering::Less),
+                    _ => Some(Ordering::Equal),
+                }
+            }
+            Some(ord) => Some(ord),
+            _ => None,
+        }
+    }
+}
+
+impl Ord for ModVersion {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.version.cmp(&other.version) {
+            Ordering::Equal => {
+                match (&self.structure, &other.structure) {
+                    (ModStructure::Zip, ModStructure::Directory) | (ModStructure::Zip, ModStructure::Symlink) => Ordering::Less,
+                    _ => Ordering::Equal,
+                }
+            }
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Less => Ordering::Less,
+        }
+    }
+}
+
+impl PartialEq for ModVersion {
+    fn eq(&self, other: &Self) -> bool {
+        self.version == other.version
+    }
+}
+
+impl Eq for ModVersion {}
+
+#[derive(Debug)]
+pub enum ModStructure {
+    Directory,
+    Symlink,
+    Zip,
+}
+
+impl ModStructure {
+    fn parse(entry: &DirEntry) -> Result<Self, ModDataErr> {
+        let path = entry.path();
+        let extension = path.extension();
+
+        if extension.is_some() && extension.unwrap() == OsStr::new("zip") {
+            return Ok(ModStructure::Zip);
+        } else {
+            let file_type = entry.file_type().map_err(|_| ModDataErr::FilesystemError)?;
+            if file_type.is_symlink() {
+                return Ok(ModStructure::Symlink);
+            } else {
+                let mut path = entry.path();
+                path.push("info.json");
+                if path.exists() {
+                    return Ok(ModStructure::Directory);
+                }
+            }
+        }
+
+        Err(ModDataErr::InvalidModStructure)
+    }
+}
+
+// Structs for deserializing json files
+#[derive(Deserialize, Debug)]
+pub struct InfoJson {
+    dependencies: Option<Vec<String>>,
+    name: String,
+    version: Version,
+}
+
+#[derive(Deserialize)]
+pub struct ModListJson {
+    mods: Vec<ModListJsonMod>,
+}
+
+#[derive(Deserialize)]
+struct ModListJsonMod {
+    name: String,
+    enabled: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,21 @@
  */
 
 mod dependency;
+mod factorio_concepts;
 mod modloader;
 
-use crate::modloader::ModLoader;
-use std::cmp::Ordering;
-use zip::ZipArchive;
-use std::fs::File;
-use std::ffi::OsStr;
+use std::fs;
+use std::fs::{File, DirEntry};
+use std::io::Read;
 use std::collections::HashMap;
 use std::path::{PathBuf};
-use std::fs::DirEntry;
-use std::fs;
-use std::io::Read;
 use std::error::Error;
 use thiserror::Error;
-use serde::Deserialize;
-use semver::Version;
-use lexical_sort::natural_only_alnum_cmp;
+use zip::ZipArchive;
 
 use crate::dependency::{ModDependency, ModDependencyResult, ModDependencyType};
+use crate::factorio_concepts::{ModVersion, ModStructure, Mod, ModListJson, ModEnabledType, InfoJson};
+use crate::modloader::ModLoader;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Mods directory path. Contains mod files/dirs, mod-list.json and mod-settings.dat
@@ -211,166 +207,4 @@ pub enum ModDataErr {
     ModDoesNotExist,
     #[error("Incompatible mods: {0} is incompatible with {1}")]
     IncompatibleMods(String, String),
-}
-
-// enum for states of a mod (enabled or disabled)
-#[derive(Debug)]
-pub enum ModEnabledType {
-    Disabled,
-    Latest,           // Legacy from factorio_mod_manager, probably will be renamed
-    Version(Version), // Legacy from factorio_mod_manager, probably will be removed
-}
-
-// Mod struct, containing mod name, version and enabled info
-#[derive(Debug)]
-pub struct Mod {
-    name: String,
-    version: Option<ModVersion>,
-    enabled: ModEnabledType,
-}
-
-// impls for sorting the mod list for loading order
-impl PartialOrd for Mod {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self.name == other.name {
-            return None;
-        }
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Mod {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.has_dependency(&other.name) {
-            return Ordering::Greater;
-        } else {
-            return natural_only_alnum_cmp(&self.name, &other.name);
-        }
-    }
-}
-
-impl PartialEq for Mod {
-    fn eq(&self, other: &Self) -> bool {
-        self.version == other.version && self.name == other.name
-    }
-}
-
-impl Eq for Mod {}
-
-impl Mod {
-    // Check if this mod has other mod as a dependency
-    fn has_dependency(&self, dep_name: &String) -> bool {
-        match &self.version {
-            Some(version) => {
-                for dependency in &version.dependencies {
-                    if &dependency.name == dep_name {
-                        match &dependency.dep_type {
-                            ModDependencyType::Optional | ModDependencyType::Required | ModDependencyType::OptionalHidden => true,
-                            _ => false,
-                        };
-                    }
-                }
-                false
-            },
-            _ => false,
-        }
-    }
-}
-
-// Struct for Mod version (or file, terminology isn't perfect)
-#[derive(Debug)]
-struct ModVersion {
-    entry: DirEntry,
-    dependencies: Vec<ModDependency>,
-    structure: ModStructure,
-    version: Version,
-}
-
-// impls for comparing mod versions
-impl PartialOrd for ModVersion {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match self.version.partial_cmp(&other.version) {
-            Some(Ordering::Equal) => {
-                match (&self.structure, &other.structure) {
-                    (ModStructure::Zip, ModStructure::Directory) | (ModStructure::Zip, ModStructure::Symlink) => Some(Ordering::Less),
-                    _ => Some(Ordering::Equal),
-                }
-            }
-            Some(ord) => Some(ord),
-            _ => None,
-        }
-    }
-}
-
-impl Ord for ModVersion {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.version.cmp(&other.version) {
-            Ordering::Equal => {
-                match (&self.structure, &other.structure) {
-                    (ModStructure::Zip, ModStructure::Directory) | (ModStructure::Zip, ModStructure::Symlink) => Ordering::Less,
-                    _ => Ordering::Equal,
-                }
-            }
-            Ordering::Greater => Ordering::Greater,
-            Ordering::Less => Ordering::Less,
-        }
-    }
-}
-
-impl PartialEq for ModVersion {
-    fn eq(&self, other: &Self) -> bool {
-        self.version == other.version
-    }
-}
-
-impl Eq for ModVersion {}
-
-#[derive(Debug)]
-enum ModStructure {
-    Directory,
-    Symlink,
-    Zip,
-}
-
-impl ModStructure {
-    fn parse(entry: &DirEntry) -> Result<Self, ModDataErr> {
-        let path = entry.path();
-        let extension = path.extension();
-
-        if extension.is_some() && extension.unwrap() == OsStr::new("zip") {
-            return Ok(ModStructure::Zip);
-        } else {
-            let file_type = entry.file_type().map_err(|_| ModDataErr::FilesystemError)?;
-            if file_type.is_symlink() {
-                return Ok(ModStructure::Symlink);
-            } else {
-                let mut path = entry.path();
-                path.push("info.json");
-                if path.exists() {
-                    return Ok(ModStructure::Directory);
-                }
-            }
-        }
-
-        Err(ModDataErr::InvalidModStructure)
-    }
-}
-
-// Structs for deserializing json files
-#[derive(Deserialize, Debug)]
-struct InfoJson {
-    dependencies: Option<Vec<String>>,
-    name: String,
-    version: Version,
-}
-
-#[derive(Deserialize)]
-struct ModListJson {
-    mods: Vec<ModListJsonMod>,
-}
-
-#[derive(Deserialize)]
-struct ModListJsonMod {
-    name: String,
-    enabled: bool,
 }
