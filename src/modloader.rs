cb490b96bc85a32559b0365a92584ec5f505c87e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-07-07 16:47:13 +0300

    Raw localised print

--- src/modloader.rs
+++ src/modloader.rs
@@ -1,3 +1,4 @@
+use mlua::prelude::LuaResult;
 use crate::Mod;
 use std::error::Error;
 use thiserror::Error;
@@ -14,6 +15,23 @@ pub struct ModLoader {
 impl ModLoader {
     pub fn new(mod_list: Vec<Mod>) -> Result<Self, ModLoaderErr> {
         let lua = mlua::Lua::new();
+
+        {
+            let globals = lua.globals();
+
+            // TODO: actual functionality
+            fn localised_print(_callback_lua: &mlua::Lua, data: mlua::Value) -> LuaResult<()> {
+                match data {
+                    mlua::Value::String(data_str) => println!("{}", data_str.to_str().unwrap()),
+                    mlua::Value::Table(data_table) => println!("Localised print: {:?}", data_table),
+                    _ => println!("Invalid call to localised_print")
+                }
+                Ok(())
+            }
+
+            globals.raw_set("localised_print", lua.create_function(localised_print).unwrap()).unwrap();
+        }
+
         return Ok(Self {
             lua,
             mod_list,
@@ -30,5 +48,5 @@ pub enum ModLoaderErr {
     #[error("Mod Loader Error")]
     GeneralError,
     #[error("Failed to load lualib")]
-    LuaLibLoadError
+    LuaLibLoadError,
 }
