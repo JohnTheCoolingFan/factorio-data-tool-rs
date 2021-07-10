cb9f3b02f83320eb2d9992466ec84cce788141a2
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-07-10 18:16:23 +0300

    Fixed imports

--- src/ini_serde.rs
+++ src/ini_serde.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize, Visitor};
+use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 
 // I'm not using serde-ini (https://github.com/arcnmx/serde-ini) because:
